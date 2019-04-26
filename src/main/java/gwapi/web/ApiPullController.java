069b935705d52b23205fb3eb77db88f2280651ca
karlviik <viik.karl2@gmail.com>
2019-04-26 23:06:30 +0300

    Added mapping to be able to access items with /item?id=x

--- src/main/java/gwapi/web/ApiPullController.java
+++ src/main/java/gwapi/web/ApiPullController.java
@@ -1,14 +1,23 @@
 package gwapi.web;
 
+import gwapi.dao.ItemDao;
+import gwapi.entity.Item;
 import gwapi.service.ItemUpdateService;
 import gwapi.service.PriceUpdateService;
 import gwapi.service.RecipeUpdateService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+import java.util.Optional;
+
+
+@Controller
 public class ApiPullController {
 
   @Autowired
@@ -23,10 +32,39 @@ public class ApiPullController {
   @Autowired
   private ItemUpdateService itemUpdateService;
 
-  @GetMapping("/hello2")
+  @Autowired
+  private ItemDao itemDao;
+
+  @GetMapping("/item")
+  public String item(Model model, @RequestParam(value="id", required=false, defaultValue="84") String idString) {
+    Integer id;
+    try {
+      id = Integer.parseInt(idString);
+    } catch (NumberFormatException e) {
+      return "404";
+    }
+    Optional<Item> optionalItem = itemDao.getItemNoComponents(id);
+    if (optionalItem.isEmpty()) {
+      return "404";
+    }
+    Item item = optionalItem.get();
+    model.addAttribute("name", item.getName());
+    model.addAttribute("id", id);
+    model.addAttribute("level", item.getLevel().toString());
+    model.addAttribute("rarity", item.getRarity().name());
+    model.addAttribute("type", item.getType().name());
+    model.addAttribute("chat_link", item.getChatLink());
+    model.addAttribute("bound", item.getBound().toString());
+    model.addAttribute("vendor", item.getVendorValue().toString());
+    model.addAttribute("icon", item.getIconLink());
+
+    return "item";
+  }
+
+  @GetMapping("/itemm")
   public void fetchItems() {
-    recipeUpdateService.addNewRecipes();
-    priceUpdateService.updatePrices();
+//    recipeUpdateService.addNewRecipes();
+//    priceUpdateService.updatePrices();
   }
 
 }
